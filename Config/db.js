3147a44803b43a9509a0549c5ee803488ffa43b8
vepada-sateesh <sateeshvepadas@gmail.com>
2023-01-27 15:26:44 +0530

    added

--- Config/db.js
+++ Config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
-const connection = mongoose.connect(Process.env.MONGO_URL)
+const connection = mongoose.connect(process.env.MONGO_URL)
 
 module.exports = {connection}
